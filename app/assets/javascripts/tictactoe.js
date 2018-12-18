e7fc57f114dd4fa3aa2c3e60e624ba7421ca228b
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-12-18 23:42:30 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -110,7 +110,7 @@ function saveGame() {
 }
 
 function updatedAt(game_data) {
-  date = new Date(game-data.attributes['updated-at'])
+  date = new Date(game_data.attributes['updated-at'])
   debugger
 }
 
